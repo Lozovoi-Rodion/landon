158f415ba209c00e5a6577dc448ed76cc6f03207
Rodion Lozovoi <rodionlozovoi@gmail.com>
2023-04-21 22:19:01 +0200

    v3 aws sdk added for creating 4 dynamodb tables

---
+++ src/scripts/CreateServicesTable.js
@@ -0,0 +1,27 @@
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+
+const region = "us-east-1";
+
+const dynamodb = new DynamoDB({ region });
+
+const params = {
+  TableName: "Services",
+  KeySchema: [
+    // Partition Key
+    { AttributeName: "name", KeyType: "HASH" }  
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "name", AttributeType: "S" }
+  ], 
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+};
+
+dynamodb.createTable(params, function(err, data) {
+  if (err)
+    console.error("Unable to create table: ", JSON.stringify(err, null, 2))
+  else
+    console.log("Created table with description: ", JSON.stringify(data, null, 2))
+});
